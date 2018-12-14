68384b304dd2dddfddd140aa5c5135635434bf41
Eric Schwartz <eric@techtalentsouth.com>
2018-12-14 11:19:40 -0500

    changed this file to prove a point

---
+++ main.js
@@ -0,0 +1,53 @@
+// let person = {
+//     firstName: "eric",
+//     lastName: "Schwartz",
+//     DOB: "9/30",
+//     sayName: function(){
+//         return this.firstName + " " + this.lastName;
+//     }
+// }
+
+//prior to es6
+// function person(firstName, lastName, DOB){
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.DOB = DOB;
+//     this.sayName = function(){
+//         return this.firstName + " " + this.lastName;
+//     }
+// }
+
+class person {
+    
+    constructor(firstName, lastName, DOB){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.DOB = DOB;
+        
+    }
+
+    sayName(){
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+let myPerson = new person("eric", "schwartz", "9/30");
+let myPerson2 = new person("Bob", "Smith", "9/31");
+
+let people = [myPerson, myPerson2];
+
+// for(let i = 0; i < people.length; i++ ){
+//     console.log(people[i].sayName());
+// }
+
+let i =0;
+for(let person of people){
+    console.log(person.sayName());
+    i++;
+}
+
+let i =0;
+while(i<people.length){
+    console.log(people[i].sayName());
+    i++
+}
